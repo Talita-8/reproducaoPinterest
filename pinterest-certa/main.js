c44c529fd26db1097cf8f42da32990b470dd58c6
Talita-8 <tlthmf1@outlook.com>
2020-12-06 22:14:12 -0300

    alteração de arquivos

---
+++ pinterest-certa/main.js
@@ -0,0 +1,11 @@
+
+const galeryPics = document.getElementById("galery-container")
+const textIn = document.getElementById("in-box")
+textIn.addEventListener("keyup", showPics)
+
+function showPics(){
+    var key = e.which || e.keyCode
+    if (key == 13){
+        
+    }
+}
