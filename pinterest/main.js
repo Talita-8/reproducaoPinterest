95e8ecc07f2a54b0bda471e97782024cf2e0a78d
Talita-8 <tlthmf1@outlook.com>
2020-12-07 00:11:29 -0300

    Incluido arquivo Data

---
+++ pinterest/main.js
@@ -0,0 +1,22 @@
+import { gallery } from './data.js'
+
+console.log(gallery)
+
+const galleryPics = document.getElementById("gallery-container")
+let textIn = document.getElementById("in-box")
+textIn.addEventListener("keyup", showPics)
+
+function showPics(){
+    let countin = 0
+    let pics = ""
+    let world = textIn.value
+    const propertyData = Object.keys(gallery)
+    const stringData = propertyData.toString()
+
+    if(world.toUpperCase() == stringData.toUpperCase()){
+        for(let i of gallery.Café)
+        pics += `<img src="${i}" id="pic-${countin += 1} class="pictures" `
+    }
+    galleryPics.innerHTML = pics
+}
+
