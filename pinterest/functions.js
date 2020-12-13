45f36c3ff821ee1f1fdffae040050c57ef2bff94
Talita-8 <tlthmf1@outlook.com>
2020-12-13 20:22:15 -0300

    docs/Botões e galeria atualizados

--- pinterest/functions.js
+++ pinterest/functions.js
@@ -1,5 +1,5 @@
 
-export const filterText = function textFunc(data, inputText){
+  export const filterText = function textFunc(data, inputText){
     let category = Object.entries(data)
     return category.filter(search => search.includes(inputText))
   }
